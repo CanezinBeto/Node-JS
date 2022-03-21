bde2ea8d1fab4a41e5a7dd576ac25aba6c50825d
CanezinBeto <albertocanezin@gmail.com>
2022-03-21 19:44:29 -0300

    Métodos de requisições

--- Node-Express/install-express/index.js
+++ Node-Express/install-express/index.js
@@ -4,7 +4,31 @@ const app = express()
 const PORT = 5000
 
 // Servindo arquivos estáticos
-app.use(express.static(path.join(__dirname, 'client')))
+app.use('/meusite', express.static(path.join(__dirname, 'client')))
+
+// Método GET
+app.get('/', (req, res) => {
+  res.type('html')
+  res.send('<h1>Hello World from GET</h1>')
+})
+
+// Método POST
+app.post('/', (req, res) => {
+  res.type('html')
+  res.send('<h1>Hello World from POST</h1>')
+})
+
+// Método PUT
+app.put('/', (req, res) => {
+  res.type('html')
+  res.send('<h1>Hello World from PUT</h1>')
+})
+
+// Método DELETE
+app.delete('/', (req, res) => {
+  res.type('html')
+  res.send('<h1>Hello World from DELETE</h1>')
+})
 
 app.listen(PORT, () => {
   console.log('Server Running on Port: ' + PORT)
