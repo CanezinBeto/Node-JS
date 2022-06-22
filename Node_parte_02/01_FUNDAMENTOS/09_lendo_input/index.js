72dd7bc6882c8a3c38891f3061234125f73bda93
CanezinBeto <albertocanezin@gmail.com>
2022-06-22 10:21:38 -0300

    Lendo input de dados

---
+++ Node_parte_02/01_FUNDAMENTOS/09_lendo_input/index.js
@@ -0,0 +1,10 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+readline.question('Qual a sua linguagem preferida? ', (language) => {
+  if ('Python') console.log('Isso nem é linguagem!!')
+  else console.log(`A minha linguagem preferida é: ${language}`)
+  readline.close()
+})
