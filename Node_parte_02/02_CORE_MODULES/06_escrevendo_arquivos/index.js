ca0312401619cd97a9a9268161b5b14f92266085
CanezinBeto <albertocanezin@gmail.com>
2022-06-24 09:45:28 -0300

    Escrevendo em arquivos por formulários HTML

---
+++ Node_parte_02/02_CORE_MODULES/06_escrevendo_arquivos/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const http = require('http')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  const urlInfo = require('url').parse(req.url, true)
+  const name = urlInfo.query.name
+
+  if (!name) {
+    fs.readFile('index.html', (error, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.write(data)
+      return res.end()
+    })
+  } else {
+    fs.writeFile('arquivo.txt', name, (error, data) => {
+      res.writeHead(302, {
+        location: '/',
+      })
+      return res.end()
+    })
+  }
+})
+
+server.listen(port, () => {
+  console.log(`Server running in port: ${port}`)
+})
